3b4e0f2ba204e83c5e7741f5f4f92f4fe604bf41
Jeffrey V <jeffreyvicente@hotmail.com>
2023-05-23 21:55:50 -0700

    Working project, no comments

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 //// Runs the application using imports from lib/
 const inquirer = require("inquirer");
 
+const fs = require('fs');
+
+
 const questionPrompts = [
     {
         type:"input",
@@ -35,11 +38,60 @@ const questionPrompts = [
 function init(){
     inquirer.prompt(questionPrompts).then((answers) => {
 
-        const shape = require('./lib/shapes.js');
-        const temp = shape.Shapes
+        /*
+        console.log(answers.logoShape);
 
+        const { Shape, Triangle } = require('./lib/shapes');
+        //const triangle = new Triangle('red');
+        const shape = new Shape('red');
+        const triangleMarkup = shape.render();
+        console.log(triangleMarkup);
+        */
+
+    const {Triangle, Circle, Shape } = require('./lib/shapes');
+
+    let shape;
+
+    switch (answers.logoShape) {
+    case 'Circle':
+        shape = new Circle(answers.logoShapeColor,answers.logoTextColor, answers.logoText);
+        break;
+    case 'Triangle':
+        shape = new Triangle(answers.logoShapeColor,answers.logoTextColor, answers.logoText);
+        break;
+    case 'Square':
+        shape = new Shape(answers.logoShapeColor,answers.logoTextColor, answers.logoText);
+        break;
+    default:
+        console.log('Invalid shape selection.');
+        return;
+    }
+
+    const shapeTemp = shape.render();
+    console.log(shapeTemp);
+
+    writeToFile("./examples/newShape.svg" , generateShape(shapeTemp))
+        
     });
 
+}
+
+function generateShape(shape) {
+
+    return `
+    <svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+         ${shape}
+         
+    </svg>
+    `;
+}
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+    // prints out error message if the file cannot be generate.
+    // prints out successful message if the file generates. 
+    err ? console.error(err) : console.log('File generation is successful!')
+    );
 }
 
 init();
